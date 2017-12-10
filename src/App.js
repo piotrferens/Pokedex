366f1f8809ac9a4c7971788ba268a7b2ff03c512
Piotr Ferens <piotrferens@outlook.com>
2017-12-10 20:38:36 +0100

    fetching and rendering pokemon name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    pokemons: [],
+    isLoading: false
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=784")
+      .then(response => response.json())
+      .then(response =>
+        this.setState({ pokemons: response.results, isLoading: false })
+      );
+  }
   render() {
-    return <div className="App">ELDO</div>;
+    return (
+      <div>
+        {this.state.isLoading ? <p>Loading...</p> : null}
+        {this.state.pokemons.map(pokemon => (
+          <p key={pokemon.name}>{pokemon.name}</p>
+        ))}
+      </div>
+    );
   }
 }
 
